0afe0e32292b13952bacfc7f7e06e1ffb6c8dd88
Adam Reed <kusa.xisa@gmail.com>
2018-01-02 19:14:18 -0500

    Added hand evaluation

--- lib/deck.rb
+++ lib/deck.rb
@@ -40,4 +40,187 @@ class Deck
       "#{short}#{card[:suit][0].downcase}"
     end
   end
+
+  def hand_value(cards:)
+    sorted_cards = sort_rank cards: cards
+    ranks = get_ranks cards: sorted_cards
+
+    # Straight Flush check
+    flush_cards = check_flush cards: sorted_cards
+    straight_flush_cards = check_straight cards: flush_cards if flush_cards
+
+    if straight_flush_cards
+      return { type: 'Straight Flush',
+               high: straight_flush_cards.last[:rank] }
+    end
+
+    # Four of a Kind check
+    quads = check_four_of_a_kind ranks: ranks
+
+    if quads
+      kickers = find_kickers ranks: ranks, high: quads, count: 1
+      return { type: 'Four of a Kind',
+               high: quads,
+               kickers: kickers }
+    end
+
+    # Full House check
+    trips, pair = check_full_house ranks: ranks
+
+    if trips && pair
+      return { type: 'Full House',
+               high: trips,
+               low: pair }
+    end
+
+    # Flush check
+    if flush_cards
+      ranks = get_ranks cards: flush_cards
+      high = ranks.last
+      kickers = find_kickers ranks: ranks, high: high, count: 4
+      return { type: 'Flush',
+               high: high,
+               kickers: kickers }
+    end
+
+    # Straight check
+    straight_cards = check_straight cards: sorted_cards
+
+    if straight_cards
+      return { type: 'Straight',
+               high: straight_cards.last[:rank] }
+    end
+
+    # Three of a Kind check
+    if trips
+      kickers = find_kickers ranks: ranks, high: trips, count: 2
+
+      return { type: 'Three of a Kind',
+               high: trips,
+               kickers: kickers }
+    end
+
+    # Two Pair check
+    two = check_two_pair ranks: ranks, pair: pair
+
+    if two
+      kickers = find_kickers ranks: ranks, high: pair, low: two, count: 1
+      return { type: 'Two Pair',
+               high: pair,
+               low: two,
+               kickers: kickers }
+    end
+
+    # One Pair check
+    if pair
+      kickers = find_kickers ranks: ranks, high: pair, count: 3
+      return { type: 'One Pair',
+               high: pair,
+               kickers: kickers }
+    end
+
+    # High Card
+    high = ranks.last
+    kickers = find_kickers ranks: ranks, high: high, count: 4
+
+    { type: 'High Card',
+      high: high,
+      kickers: kickers }
+  end
+
+  def check_flush(cards:)
+    suit_counters = { 'Clubs' => 0, 'Diamonds' => 0,
+                      'Hearts' => 0, 'Spades' => 0 }
+
+    cards.each do |card|
+      suit = card[:suit]
+      suit_counters[suit] += 1
+      return flush_cards cards: cards, suit: suit if suit_counters[suit] >= 5
+    end
+
+    nil
+  end
+
+  def flush_cards(cards:, suit:)
+    cards.select { |card| card[:suit] == suit }
+  end
+
+  def check_straight(cards:)
+    unique_cards = remove_pairs cards: cards
+    unique_cards.unshift rank: 1 if hand_has_ace? cards: cards
+    start = unique_cards.length - 5
+
+    start.downto 0 do |first_card|
+      potential_hand = unique_cards[first_card..first_card + 4]
+      return potential_hand if straight? cards: potential_hand
+    end
+
+    nil
+  end
+
+  def check_four_of_a_kind(ranks:)
+    0.upto ranks.length - 3 do |rank|
+      return ranks[rank] if ranks.count(ranks[rank]) == 4
+    end
+
+    nil
+  end
+
+  def check_full_house(ranks:)
+    trips = pair = nil
+    start = ranks.length - 2
+
+    start.downto 1 do |rank|
+      card = ranks[rank]
+
+      trips = card if ranks.count(card) == 3 && !trips
+      pair = card if ranks.count(card) >= 2 && card != trips && !pair
+    end
+
+    [trips, pair]
+  end
+
+  def check_two_pair(ranks:, pair:)
+    return unless pair
+
+    two = nil
+
+    0.upto ranks.length - 3 do |rank|
+      two = ranks[rank] if ranks.count(ranks[rank]) == 2 && ranks[rank] != pair
+    end
+
+    two
+  end
+
+  def remove_pairs(cards:)
+    cards.uniq { |card| card[:rank] }
+  end
+
+  def hand_has_ace?(cards:)
+    cards.find { |card| card[:rank] == 14 }
+  end
+
+  def sort_rank(cards:)
+    cards.sort { |a, b| a[:rank] <=> b[:rank] }
+  end
+
+  def straight?(cards:)
+    ranks = get_ranks cards: cards
+
+    ranks == (ranks[0]..ranks[4]).to_a
+  end
+
+  def get_ranks(cards:)
+    cards.map { |card| card[:rank] }
+  end
+
+  def find_kickers(ranks:, count:, high:, low: 0)
+    kickers = []
+
+    ranks.reverse.each do |rank|
+      kickers << rank if rank != high && rank != low && kickers.length < count
+    end
+
+    kickers
+  end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -5,6 +5,23 @@ describe Deck do
     @deck = Deck.new
   end
 
+  before :all do
+    def fill_rank(rank:, cards:)
+      cards[rank] = {}
+      suits = %w(Clubs Diamonds Hearts Spades)
+
+      4.times do |suit|
+        cards[rank][suits[suit][0]] = { rank: rank, suit: suits[suit] }
+      end
+    end
+
+    @cards = []
+
+    2.upto(14) do |rank|
+      fill_rank rank: rank, cards: @cards
+    end
+  end
+
   describe '#new' do
     it 'fills a deck with cards' do
       expect(@deck.cards.length).to eq 52
@@ -23,28 +40,159 @@ describe Deck do
   end
 
   describe '#to_s' do
-    before do
-      @ace_of_spades = { rank: 14, suit: 'Spades' }
-      @two_of_clubs = { rank: 2, suit: 'Clubs' }
-      @ten_of_hearts = { rank: 10, suit: 'Hearts' }
-    end
-
     context 'When full is specified' do
       it 'gives the full name of a card' do
-        expect(@deck.to_s(card: @ace_of_spades, form: 'Full'))
+        expect(@deck.to_s(card: @cards[14]['S'], form: 'Full'))
           .to eq 'Ace of Spades'
-        expect(@deck.to_s(card: @two_of_clubs, form: 'Full'))
+        expect(@deck.to_s(card: @cards[2]['C'], form: 'Full'))
           .to eq 'Two of Clubs'
-        expect(@deck.to_s(card: @ten_of_hearts, form: 'Full'))
+        expect(@deck.to_s(card: @cards[10]['H'], form: 'Full'))
           .to eq 'Ten of Hearts'
       end
     end
 
     context 'When no form is specified' do
       it 'gives the short name of a card' do
-        expect(@deck.to_s(card: @ace_of_spades)).to eq 'As'
-        expect(@deck.to_s(card: @two_of_clubs)).to eq '2c'
-        expect(@deck.to_s(card: @ten_of_hearts)).to eq 'Th'
+        expect(@deck.to_s(card: @cards[14]['S'])).to eq 'As'
+        expect(@deck.to_s(card: @cards[2]['C'])).to eq '2c'
+        expect(@deck.to_s(card: @cards[10]['H'])).to eq 'Th'
+      end
+    end
+  end
+
+  describe '#hand_value' do
+    context 'when given a Straight Flush' do
+      it 'reports a Straight Flush to the appropriate high' do
+        one = [@cards[14]['S'], @cards[2]['S'], @cards[3]['S'], @cards[4]['S'],
+               @cards[5]['S'], @cards[8]['S'], @cards[11]['S']]
+        one_expected = { type: 'Straight Flush', high: 5 }
+
+        two = [@cards[11]['S'], @cards[12]['S'], @cards[9]['S'], @cards[4]['S'],
+               @cards[10]['S'], @cards[13]['S'], @cards[14]['S']]
+        two_expected = { type: 'Straight Flush', high: 14 }
+
+        expect(@deck.hand_value(cards: one)).to eq one_expected
+        expect(@deck.hand_value(cards: two)).to eq two_expected
+      end
+    end
+
+    context 'when given a Four of a Kind' do
+      it 'reports a Four of a Kind of the appropriate values' do
+        one = [@cards[5]['C'], @cards[5]['D'], @cards[5]['H'], @cards[5]['S'],
+               @cards[14]['S'], @cards[8]['S'], @cards[11]['S']]
+        one_expected = { type: 'Four of a Kind', high: 5, kickers: [14] }
+
+        two = [@cards[14]['C'], @cards[14]['D'], @cards[5]['H'], @cards[5]['S'],
+               @cards[14]['H'], @cards[14]['S'], @cards[13]['S']]
+        two_expected = { type: 'Four of a Kind', high: 14, kickers: [13] }
+
+        expect(@deck.hand_value(cards: one)).to eq one_expected
+        expect(@deck.hand_value(cards: two)).to eq two_expected
+      end
+    end
+
+    context 'when given a Full House' do
+      it 'reports a Full House of the appropriate values' do
+        one = [@cards[5]['C'], @cards[5]['D'], @cards[5]['H'], @cards[6]['S'],
+               @cards[6]['D'], @cards[8]['S'], @cards[11]['S']]
+        one_expected = { type: 'Full House', high: 5, low: 6 }
+
+        two = [@cards[5]['C'], @cards[5]['D'], @cards[5]['H'], @cards[14]['C'],
+               @cards[14]['H'], @cards[14]['S'], @cards[13]['S']]
+        two_expected = { type: 'Full House', high: 14, low: 5 }
+
+        expect(@deck.hand_value(cards: one)).to eq one_expected
+        expect(@deck.hand_value(cards: two)).to eq two_expected
+      end
+    end
+
+    context 'when given a Flush' do
+      it 'reports a Flush of the appropriate values' do
+        one = [@cards[2]['C'], @cards[3]['C'], @cards[4]['C'], @cards[6]['C'],
+               @cards[14]['C'], @cards[8]['S'], @cards[11]['S']]
+        one_expected = { type: 'Flush', high: 14, kickers: [6, 4, 3, 2] }
+
+        two = [@cards[2]['D'], @cards[11]['D'], @cards[9]['D'], @cards[12]['D'],
+               @cards[6]['D'], @cards[8]['D'], @cards[5]['D']]
+        two_expected = { type: 'Flush', high: 12, kickers: [11, 9, 8, 6] }
+
+        expect(@deck.hand_value(cards: one)).to eq one_expected
+        expect(@deck.hand_value(cards: two)).to eq two_expected
+      end
+    end
+
+    context 'when given a Straight' do
+      it 'reports a Straight to the appropriate high' do
+        one = [@cards[2]['C'], @cards[3]['C'], @cards[4]['C'], @cards[5]['C'],
+               @cards[12]['D'], @cards[14]['S'], @cards[11]['S']]
+        one_expected = { type: 'Straight', high: 5 }
+
+        two = [@cards[8]['D'], @cards[9]['D'], @cards[10]['D'], @cards[10]['C'],
+               @cards[11]['C'], @cards[11]['S'], @cards[7]['H']]
+        two_expected = { type: 'Straight', high: 11 }
+
+        expect(@deck.hand_value(cards: one)).to eq one_expected
+        expect(@deck.hand_value(cards: two)).to eq two_expected
+      end
+    end
+
+    context 'when given a Three of a Kind' do
+      it 'reports a Three of a Kind of the appropriate values' do
+        one = [@cards[2]['C'], @cards[3]['C'], @cards[4]['C'], @cards[5]['C'],
+               @cards[5]['D'], @cards[5]['S'], @cards[13]['S']]
+        one_expected = { type: 'Three of a Kind', high: 5, kickers: [13, 4] }
+
+        two = [@cards[8]['D'], @cards[9]['D'], @cards[12]['D'], @cards[13]['C'],
+               @cards[14]['C'], @cards[14]['S'], @cards[14]['H']]
+        two_expected = { type: 'Three of a Kind', high: 14, kickers: [13, 12] }
+
+        expect(@deck.hand_value(cards: one)).to eq one_expected
+        expect(@deck.hand_value(cards: two)).to eq two_expected
+      end
+    end
+
+    context 'when given a Two Pair' do
+      it 'reports a Two Pair of the appropriate values' do
+        one = [@cards[6]['C'], @cards[6]['H'], @cards[5]['C'], @cards[5]['D'],
+               @cards[8]['D'], @cards[12]['S'], @cards[14]['S']]
+        one_expected = { type: 'Two Pair', high: 6, low: 5, kickers: [14] }
+
+        two = [@cards[8]['D'], @cards[8]['H'], @cards[9]['D'], @cards[9]['C'],
+               @cards[14]['C'], @cards[14]['S'], @cards[7]['H']]
+        two_expected = { type: 'Two Pair', high: 14, low: 9, kickers: [8] }
+
+        expect(@deck.hand_value(cards: one)).to eq one_expected
+        expect(@deck.hand_value(cards: two)).to eq two_expected
+      end
+    end
+
+    context 'when given a One Pair' do
+      it 'reports a One Pair of the appropriate values' do
+        one = [@cards[6]['C'], @cards[4]['H'], @cards[5]['C'], @cards[5]['D'],
+               @cards[8]['D'], @cards[2]['S'], @cards[14]['S']]
+        one_expected = { type: 'One Pair', high: 5, kickers: [14, 8, 6] }
+
+        two = [@cards[2]['D'], @cards[2]['H'], @cards[3]['D'], @cards[4]['C'],
+               @cards[5]['C'], @cards[7]['S'], @cards[8]['H']]
+        two_expected = { type: 'One Pair', high: 2, kickers: [8, 7, 5] }
+
+        expect(@deck.hand_value(cards: one)).to eq one_expected
+        expect(@deck.hand_value(cards: two)).to eq two_expected
+      end
+    end
+
+    context 'when given a High Card' do
+      it 'reports a High Card of the appropriate values' do
+        one = [@cards[2]['C'], @cards[3]['H'], @cards[4]['C'], @cards[5]['D'],
+               @cards[7]['D'], @cards[8]['S'], @cards[9]['S']]
+        one_expected = { type: 'High Card', high: 9, kickers: [8, 7, 5, 4] }
+
+        two = [@cards[2]['D'], @cards[7]['H'], @cards[9]['D'], @cards[11]['C'],
+               @cards[12]['C'], @cards[13]['S'], @cards[14]['H']]
+        two_expected = { type: 'High Card', high: 14, kickers: [13, 12, 11, 9] }
+
+        expect(@deck.hand_value(cards: one)).to eq one_expected
+        expect(@deck.hand_value(cards: two)).to eq two_expected
       end
     end
   end
